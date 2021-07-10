c7ab6a52c79df8090aea15dcc188969b62bef620
QuanTranDoanAnh <td.anhquan@gmail.com>
2021-07-10 17:53:27 +0700

    Spring Boot app scaffold

---
+++ src/main/java/vn/quantda/sbrbootdemo/SbrbootdemoApplication.java
@@ -0,0 +1,13 @@
+package vn.quantda.sbrbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbrbootdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/quantda/sbrbootdemo/SbrbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.quantda.sbrbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
